91e136c19e110177bc9e23ac2f2d220c1e467040
gustawdaniel <gustaw.daniel@gmail.com>
2016-11-04 20:31:19 +0100

    Multiple products list

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Javascript exemplary repo
 
 This repo contains exemplary usage of javascript
+
+-------
+
+# Instalation
+
+```
+git clone https://github.com/gustawdaniel/javascript_training.git
+bower install
+php -S localhost:8000
+firefox localhost:8000 &
+```
--- js/site.js
+++ js/site.js
@@ -1,9 +1,39 @@
 var data = {
-    "name" : "Kurs JavaScript",
-    "price": 60,
-    "category": "internet"
+    "products" : [
+        {
+            "name" : "JavaScript",
+            "price": 60,
+            "category": "web"
+        },{
+            "name" : "Mysql",
+            "price": 55,
+            "category": "programing"
+        },{
+            "name" : "C++",
+            "price": 85,
+            "category": "programing"
+        },{
+            "name" : "MongoDB",
+            "price": 65,
+            "category": "programing"
+        }
+    ]
 };
 
-for(var key in data){
-    document.getElementById('p_'+key).innerHTML = data[key];
+// get list and container
+var list = document.getElementsByClassName('list-group')[0];
+var container = document.getElementsByClassName("container")[0];
+
+// clone list into container as many products is minus one because of one exists initially
+for(var i=0; i<data.products.length - 1; i++) {
+    container.appendChild(list.cloneNode(true));
 }
+
+// iterate over products
+data.products.forEach(function (product, i) {
+    // iterate over keys
+    for(var key in product){
+        // set inner html properly to json structure
+        document.getElementsByClassName(key)[i].innerHTML = product[key];
+    }
+});
