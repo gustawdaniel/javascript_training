52cf3a5943a5e3d46b950be8575c279934e26679
gustawdaniel <gustaw.daniel@gmail.com>
2016-11-04 21:14:52 +0100

    Json in different file. Validation if json has correct format added.

--- js/site.js
+++ js/site.js
@@ -1,39 +1,23 @@
-var data = {
-    "products" : [
-        {
-            "name" : "JavaScript",
-            "price": 60,
-            "category": "web"
-        },{
-            "name" : "Mysql",
-            "price": 55,
-            "category": "programing"
-        },{
-            "name" : "C++",
-            "price": 85,
-            "category": "programing"
-        },{
-            "name" : "MongoDB",
-            "price": 65,
-            "category": "programing"
-        }
-    ]
-};
-
 // get list and container
 var list = document.getElementsByClassName('list-group')[0];
 var container = document.getElementsByClassName("container")[0];
 
-// clone list into container as many products is minus one because of one exists initially
-for(var i=0; i<data.products.length - 1; i++) {
-    container.appendChild(list.cloneNode(true));
-}
+$.getJSON('data/data.json', function (data) {
 
-// iterate over products
-data.products.forEach(function (product, i) {
-    // iterate over keys
-    for(var key in product){
-        // set inner html properly to json structure
-        document.getElementsByClassName(key)[i].innerHTML = product[key];
+    // clone list into container as many products is minus one because of one exists initially
+    for (var i = 0; i < data.products.length - 1; i++) {
+        container.appendChild(list.cloneNode(true));
     }
+
+    // iterate over products
+    data.products.forEach(function (product, i) {
+        // iterate over keys
+        for (var key in product) {
+            // set inner html properly to json structure
+            document.getElementsByClassName(key)[i].innerHTML = product[key];
+        }
+    });
+
+}).fail(function() {
+    container.innerHTML = '<div class="alert alert-danger">Json has incorrect format or is unavailable.</div>';
 });
