0174711d17c084d6a60ecf0dcadf16aaec493d86
gustawdaniel <gustaw.daniel@gmail.com>
2019-06-07 08:39:55 +0200

    Update fine.ino

--- src/fine.ino
+++ src/fine.ino
@@ -1,17 +1,18 @@
 // stałe opisujące silniki
-#define L_DIR 4
-#define L_PWM 5
-#define R_DIR 10
-#define R_PWM 11 // todo przemyśleć ułożenie silników
+#define R_PWM 11
+#define R_DIR 12
+#define L_PWM 10
+#define L_DIR 13
 #define L_MOTOR 0
 #define R_MOTOR 1
 #define PWM_MAX 255
 
-// stałe opisujące piny sensorów
-#define SENSOR_START 0
+ // stałe opisujące piny sensorów
+#define SENSOR_START 2
 #define SENSOR_COUNT 8
 
 #define HISTORY_SIZE 100
+
 struct History 
 {
   int iter;
@@ -29,7 +30,7 @@ void readSensors();
 void clearSensors();
 double calculateError();
 
-void setup()
+ void setup()
 {
   // setup code here
   pinMode(L_DIR, OUTPUT);
@@ -37,69 +38,97 @@ void setup()
   pinMode(R_DIR, OUTPUT);
   pinMode(R_PWM, OUTPUT);
 
-  for(int i = SENSOR_START; i < SENSOR_START + SENSOR_COUNT; i++)
+   for(int i = SENSOR_START; i < SENSOR_START + SENSOR_COUNT; i++)
   {
     pinMode(i, INPUT);
   }
+
+    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 }
 
-void loop()
+ void loop()
 {
   // run code here
   readSensors();
-  clearSensors();
+  Serial.print("f|");
+  printSensors();
 
-  int speed = 50; // todo dodać przyspieszanie na prostch
+  clearSensors();
+//  printSensors();
+
+   int speed = 50; // todo dodać przyspieszanie na prostch
   double turn = calculateError();
 
-  setMotorSpeed(L_MOTOR, speed - (int)turn);
-  setMotorSpeed(R_MOTOR, speed + (int)turn);
+printMotors(speed, turn);
+
+//setMotorSpeed(L_MOTOR, 90);
+//  setMotorSpeed(R_MOTOR, 90);
+   setMotorSpeed(L_MOTOR, speed + (int)turn);
+  setMotorSpeed(R_MOTOR, speed - (int)turn);
 }
 
-double sensorMiddle = (double)(SENSOR_COUNT - 1) / 2;
-double kp = 0.1;
+ double sensorMiddle = (double)(SENSOR_COUNT - 1) / 2;
+double kp = 10;
 double Ti = 1;
 double Td = 1;
 double calculateError()
 {
   double errorValue = 0.0;
-  
-  for(int i = 0; i < SENSOR_COUNT; i++)
+
+   for(int i = 0; i < SENSOR_COUNT; i++)
   {
     errorValue += (double)sensors[i] * ((double)i - sensorMiddle);
   }
 
-  historyPush(errorValue); 
+   historyPush(errorValue); 
 
-  double integral = lastErr.sum / lastErr.filled;
+   double integral = lastErr.sum / lastErr.filled;
 
-  int prevIter = (iter - 1) % HISTORY_SIZE;
+   int prevIter = (lastErr.iter - 1) % HISTORY_SIZE;
   if(prevIter < 0) prevIter += HISTORY_SIZE;
   double derivative = lastErr.history[lastErr.iter] - lastErr.history[prevIter];
 
-  return kp * (errorValue + (Ti * integral) + (Td * derivative));
+   return kp * (errorValue + (Ti * integral) + (Td * derivative));
 }
 
-void clearSensors()
+
+void printSensors()
+{
+  for(int i = SENSOR_START; i < SENSOR_START + SENSOR_COUNT; i++)
+  {
+    Serial.print(sensors[i - SENSOR_START]);  
+  }
+  Serial.println("");
+}
+
+void printMotors(int speed, double turn) {
+    Serial.print("l/" + (String) (speed + (int)turn)
+    + "r/" + (String) (speed - (int)turn));  
+  
+}
+
+ void clearSensors()
 {
   int peaks = 0;
-  for(int i=0; i<SENSORS_COUNT; i++)
+  for(int i=0; i<SENSOR_COUNT; i++)
   {
-    while(i < SENSORS_COUNT && sensors[i] == LOW) i++;
-    while(i < SENSORS_COUNT && sensord[i] != LOW) i++;
-    
-    if(sensors[i-1] != LOW)
+    while(i < SENSOR_COUNT && sensors[i] == LOW) i++;
+    while(i < SENSOR_COUNT && sensors[i] != LOW) i++;
+
+     if(sensors[i-1] != LOW)
       peaks++;
   }
 
-  if(peaks >= 2) // todo rozwarzyć naprawienie tego dla peaks >= 3
+  Serial.print((String)peaks + "|");
+
+   if(peaks >= 2) // todo rozwarzyć naprawienie tego dla peaks >= 3
   {
     if(lastErr.history[lastErr.iter] > 0) // todo sprawdzić czy to na pewno w lewo/prawo
     {
-      for(int i=0; i<SENSORS_COUNT; i++)
+      for(int i=0; i<SENSOR_COUNT; i++)
       {
-        while(i < SENSORS_COUNT && sensors[i] == LOW) i++;
-        while(i < SENSORS_COUNT && sensors[i] != LOW)
+        while(i < SENSOR_COUNT && sensors[i] == LOW) i++;
+        while(i < SENSOR_COUNT && sensors[i] != LOW)
         {
           sensors[i] = 0;
           i++;
@@ -108,7 +137,7 @@ void clearSensors()
     }
     else
     {
-      for(int i=SENSORS_COUNT; i>0; i--)
+      for(int i=SENSOR_COUNT; i>0; i--)
       {
         while(i > 0 && sensors[i] == LOW) i--;
         while(i > 0 && sensors[i] != LOW)
@@ -121,7 +150,7 @@ void clearSensors()
   }
 }
 
-void readSensors()
+ void readSensors()
 {
   for(int i = SENSOR_START; i < SENSOR_START + SENSOR_COUNT; i++)
   {
@@ -129,18 +158,18 @@ void readSensors()
   }
 }
 
-void setMotorSpeed(int motor, int speed)
+ void setMotorSpeed(int motor, int speed)
 {
   if(motor == L_MOTOR) // lewy silnik
   {
     if(speed < 0)
     {
-      digitalWrite(L_DIR, LOW); // todo sprawdzić czy się zgadza LOW/HIGH
+      digitalWrite(L_DIR, HIGH); // todo sprawdzić czy się zgadza LOW/HIGH
       speed *= -1;
     }
     else
     {
-      digitalWrite(L_DIR, HIGH);
+      digitalWrite(L_DIR, LOW);
     }
     int pwm_rate = map(speed, 0, 100, 0, PWM_MAX);
     analogWrite(L_PWM, pwm_rate);
@@ -149,28 +178,28 @@ void setMotorSpeed(int motor, int speed)
   {
     if(speed < 0)
     {
-      digitalWrite(R_DIR, LOW); // todo sprawdzić czy się zgadza LOW/HIGH
+      digitalWrite(R_DIR, HIGH); // todo sprawdzić czy się zgadza LOW/HIGH
       speed *= -1;
     }
     else
     {
-      digitalWrite(R_DIR, HIGH);
+      digitalWrite(R_DIR, LOW);
     }
     int pwm_rate = map(speed, 0, 100, 0, PWM_MAX);
     analogWrite(R_PWM, pwm_rate);
   }
 }
 
-void historyPush(double val)
+ void historyPush(double val)
 {
   lastErr.iter++;
   if(lastErr.iter == HISTORY_SIZE)
     lastErr.iter -= HISTORY_SIZE;
 
-  lastErr.sum -= lastErr.history[lastErr.iter];
+   lastErr.sum -= lastErr.history[lastErr.iter];
   lastErr.history[lastErr.iter] = val;
   lastErr.sum += lastErr.history[lastErr.iter];
 
-  if(lastErr.filled < HISTORY_SIZE)
+   if(lastErr.filled < HISTORY_SIZE)
     lastErr.filled++;
 }
